02fb10cf5d11f04940219101608dc8e17be0a465
ewkra <EW.KRASOWSKA@HOTMAIL.COM>
2019-07-27 09:02:59 +0200

    added add student controller

--- src/main/webapp/java/pl/sda/controller/AddStudentController.java
+++ src/main/webapp/java/pl/sda/controller/AddStudentController.java
@@ -1,4 +1,30 @@
 package pl.sda.controller;
 
-public class AddStudentController {
+import pl.sda.dao.StudentDao;
+import pl.sda.model.Student;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/add-student")
+public class AddStudentController extends HttpServlet {
+
+    private final StudentDao studentDao = new StudentDao();
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String lastName = req.getParameter("lastName");
+        String firstName=req.getParameter("firstName");
+        int id=Integer.parseInt(req.getParameter("id"));
+
+        studentDao.addStudent(new Student(id, firstName, lastName));
+
+        req.getRequestDispatcher("/WEB-INF/view/home.jsp").forward(req, resp);
+
+    }
 }
